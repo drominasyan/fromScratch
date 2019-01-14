a0afd1793778a0c0c7caf667266cd0ad1bb8c02e
derenik.minasyan <derominasyan94@gmail.com>
2019-01-14 18:32:01 +0400

    ok

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+class App extends React.Component {
+	render() {
+		return <div>Hello  {this.props.name}</div>;
+	}
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.png$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							mimetype: 'image/png'
+						}
+					}
+				]
+			},
+			{
+				test: /\.svg$/,
+				use: 'file-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: [
+			'.js',
+			'.jsx'
+		]
+	},
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: require('html-webpack-template'),
+			inject: false,
+			appMountId: 'app',
+		})
+
+	],
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\\/]node_modules[\\\/]/,
+					name: 'vendors',
+					chunks: 'all'
+				}
+			}
+		}
+	}
+}
+
+module.exports = config;
